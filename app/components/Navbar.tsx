2d07214e9ed5e4ef77e4988a4b77a9d4ce6c8336
johannes debritto <johanesdebrito22@gmail.com>
2025-06-23 08:45:39 +0700

    push

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -63,8 +63,8 @@ const Navbar = () => {
             { href: "#project", label: "Project" },
             { href: "#testimoni", label: "Testimoni" },
           ].map(({ href, label }) => (
-            <Link key={href} href={href} onClick={handleLinkClick}>
-              <a className="text-white text-xl hover:text-purple-500 duration-200 hover:scale-110 transition-transform">{label}</a>
+            <Link key={href} href={href} onClick={handleLinkClick} className="text-white text-xl hover:text-purple-500 duration-200 hover:scale-110 transition-transform">
+              {label}
             </Link>
           ))}
           <a
@@ -86,15 +86,12 @@ const Navbar = () => {
             { href: "#resume", label: "Resume" },
             { href: "#project", label: "Project" },
           ].map(({ href, label }) => (
-            <Link key={href} href={href} className="group">
-              <a className="text-white cursor-pointer duration-200 hover:scale-110 transition-transform hover:shadow-lg hover:text-purple-500 relative">
-                <span>{label}</span>
-                <span className="absolute -bottom-1 left-1/2 w-0 transition-all duration-200 h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                <span className="absolute -bottom-1 right-1/2 w-0 transition-all duration-200 h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-              </a>
+            <Link key={href} href={href} className="group text-white cursor-pointer duration-200 hover:scale-110 transition-transform hover:shadow-lg hover:text-purple-500 relative">
+              <span>{label}</span>
+              <span className="absolute -bottom-1 left-1/2 w-0 transition-all duration-200 h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+              <span className="absolute -bottom-1 right-1/2 w-0 transition-all duration-200 h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
             </Link>
           ))}
-
           <a
             href="https://wa.me/6285602813201"
             target="_blank"
